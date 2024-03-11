08d9f90193e08f1532506a61a24e9aad7abf1fa4
komem3 <mikuni-k@gicloud.co.jp>
2024-03-11 17:12:46 +0900

    fix script

--- init.go
+++ init.go
@@ -82,7 +82,7 @@ func Initialize() error {
 }
 
 func createScript(dir, command string) error {
-	if err := os.WriteFile(filepath.Join(dir, "bin", command), []byte("#!/bin/bash\nnvs run "+command+" -- $@\n"), 0744); err != nil {
+	if err := os.WriteFile(filepath.Join(dir, "bin", command), []byte("#!/bin/bash\nnvs run "+command+" -- \"$@\"\n"), 0744); err != nil {
 		return err
 	}
 	return nil
